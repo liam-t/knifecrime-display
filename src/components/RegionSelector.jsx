0d053bf1de5b49cc01d70fdef37a9225d931dedf
Liam T <liam.t10@icloud.com>
2020-04-11 01:23:12 +0100

    added region selector componet

---
+++ src/components/RegionSelector.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PT from 'prop-types';
+import styled from 'styled-components';
+import Select from 'react-select';
+
+const propTypes = {
+  names: PT.arrayOf(PT.string).isRequired,
+  onChange: PT.func.isRequired,
+  selectedRegion: PT.string.isRequired,
+};
+
+const RegionSelector = ({ names, onChange, selectedRegion }) => {
+  const options = names.map((name) => ({ value: name, label: name })).reverse();
+  const handleChange = ({ value }) => onChange(value);
+  return (
+    <RegionSelectorWrap>
+      <Select
+        options={options}
+        onChange={handleChange}
+        value={{
+          label: selectedRegion,
+          value: selectedRegion,
+        }}
+      />
+    </RegionSelectorWrap>
+  );
+};
+RegionSelector.propTypes = propTypes;
+
+export default RegionSelector;
+
+const RegionSelectorWrap = styled.div``;
