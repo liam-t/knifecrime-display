4ae0a0f28f47292bf90c4d0750b8502efe489e46
Liam T <liam.t10@icloud.com>
2020-05-10 17:57:39 +0100

    work on tooltip spring animation - performance opts wip

--- src/components/KnifeGraph/Tooltip.jsx
+++ src/components/KnifeGraph/Tooltip.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PT from 'prop-types';
 import styled from 'styled-components/macro';
 import pointDef from 'modeling/dataPoint';
+import { useSpring, animated } from 'react-spring';
 
 const propTypes = {
   getTimeObj: PT.func.isRequired,
@@ -24,13 +25,63 @@ const Tooltip = ({
     quarter,
     knifeCrime,
   } = selectedPoint;
-  const x = selectedPoint ? compiledXScale(getTimeObj(selectedPoint)) + 10 : 0;
-  const y = compiledYScale(knifeCrime);
+
+  const getX = (point) => (
+    point ? compiledXScale(getTimeObj(point)) + 5 : 0
+  );
+  const getY = (point) => compiledYScale(point.knifeCrime) || 0;
+
+  const [bufferedXy, setBufferedXy] = React.useState([getX(selectedPoint), getY(selectedPoint)]);
+  const [bufferedX, bufferedY] = bufferedXy;
+
+  const {
+    x: springX,
+    y: springY,
+  } = useSpring({
+    x: bufferedX,
+    y: bufferedY,
+    from: {
+      x: 0,
+      y: 0,
+    },
+  });
+
+  React.useEffect(() => {
+    setBufferedXy([getX(selectedPoint), getY(selectedPoint)]);
+  }, [selectedPoint, getX, getY]);
+
+  const width = 115;
+  const height = 60;
+  const scaleMulti = 0.8;
+  const scaledWidth = width * scaleMulti;
+  const scaledHeight = height * scaleMulti;
+  const fontSize = 16 * scaleMulti;
+
   return (
     selectedPoint ? (
       <TooltipWrap>
-        <Year x={x} y={y}>{year} &ndash; {quarter}</Year>
-        <Stat x={x} y={y} dy="1.2em">{knifeCrime} incidents</Stat>
+        <Bg
+          x={springX}
+          y={springY.interpolate((intVal) => intVal - (scaledHeight / 2))}
+          width={scaledWidth}
+          height={scaledHeight}
+        />
+        <Year
+          fontSize={fontSize}
+          x={springX.interpolate((intVal) => intVal + 10)}
+          y={springY}
+          dy="-0.2em"
+        >
+          {year} &ndash; {quarter}
+        </Year>
+        <Stat
+          fontSize={fontSize}
+          x={springX.interpolate((intVal) => intVal + 10)}
+          y={springY}
+          dy="1em"
+        >
+          {knifeCrime} incidents
+        </Stat>
       </TooltipWrap>
     ) : null
   );
@@ -40,9 +91,13 @@ Tooltip.propTypes = propTypes;
 export default Tooltip;
 
 const TooltipWrap = styled.g`
+  fill: black;
+  pointer-events: none;
+`;
+const Bg = styled(animated.rect)`
   fill: white;
 `;
-const Year = styled.text`
+const Year = styled(animated.text)`
   font-weight: bold;
 `;
-const Stat = styled.text``;
+const Stat = styled(animated.text)``;
